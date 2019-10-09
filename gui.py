9aa658b782de202d74e9a4378c4cb4c0684ba84d
Efaz Khan <efazahmed@hotmail.com>
2019-10-08 23:02:47 -0400

    added cv2 to take webcam photo

--- gui.py
+++ gui.py
@@ -1,7 +1,37 @@
 import tkinter as tk
+import cv2
+
 
 window = tk.Tk()
 window.title("Face Auth")
 window.geometry("400x400")
 
+
+def take_photo():
+	camera_port = 0 
+	ramp_frames = 30 
+	camera = cv2.VideoCapture(camera_port)
+
+	def get_image():
+		retval, im = camera.read()
+		return im 
+	for i in range(ramp_frames):
+		temp = camera.read()
+
+	camera_capture = get_image()
+	filename = "image.jpg"
+	cv2.imwrite(filename,camera_capture)
+	del(camera)
+
+
+b1 = tk.Button(window, text='take photo', command=take_photo)
+b1.pack()
+
+
 window.mainloop()
+
+
+
+
+
+
